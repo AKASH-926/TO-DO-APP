2d03d0b566ea217e3dbac9c72baa2601a15c653f
AKASH <akash.arthanur26@gmail.com>
2022-12-14 23:04:58 +0530

    added env variables

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require("express")
 
 const app = require("./app")
 const mongoose = require("mongoose")
-
+const port = process.env.PORT || 8080
 mongoose.connect("mongodb://localhost/todoDATA", () => {
     console.log("connected to DB")
 })
 
-app.listen(8080, () => {
-    console.log("server up at 8080")
+app.listen(port, () => {
+    console.log(`server up at ${port}`)
 })

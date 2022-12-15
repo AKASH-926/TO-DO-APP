751161786dee7044c6773a00cc6cd535e0ad7eda
AKASH <akash.arthanur26@gmail.com>
2022-12-15 14:05:34 +0530

    added cloud DB url

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const express = require("express")
 const app = require("./app")
 const mongoose = require("mongoose")
 const port = process.env.PORT || 8080
-mongoose.connect("mongodb://localhost/todoDATA", () => {
-    console.log("connected to DB")
-})
+mongoose.connect("mongodb+srv://AKASH:akashap@cluster0.hiv5i0f.mongodb.net/todoappdb",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }, () => {
+        console.log("connected to DB")
+    })
 
 app.listen(port, () => {
     console.log(`server up at ${port}`)
